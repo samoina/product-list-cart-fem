ea82b732afe41b6290644b51ecd49e2a71fd8066
Samoina <samoina.w@gmail.com>
2024-11-13 12:17:49 +0300

    style: style for mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,64 @@
+import products from '../data.json';
+import emptyCartIcon from './assets/images/illustration-empty-cart.svg';
+import cartIcon from './assets/images/icon-add-to-cart.svg';
+
 function App() {
 	return (
-		<>
-			<div></div>
-		</>
+		<div>
+			<h1 className="text-rose-900 font-bold text-3xl pb-3">Desserts</h1>
+			<div className="grid grid-cols-1">
+				{products.map((product) => {
+					return (
+						<div
+							className="flex flex-col gap-6 relative"
+							key={crypto.randomUUID()}
+						>
+							<picture>
+								<source
+									media="(min-width: 1024px)"
+									srcSet={product.image.desktop}
+								/>
+								<source
+									media="(min-width: 768px)"
+									srcSet={product.image.tablet}
+								/>
+								<img
+									className="rounded-lg"
+									src={product.image.mobile}
+									alt={product.name}
+								/>
+							</picture>
+
+							<button className="p-2 rounded-3xl border border-rose-300 flex justify-center font-semibold w-[50%] bg-white absolute bottom-[90px] self-center">
+								<img src={cartIcon} alt="cart icon" className="pr-2" /> Add to
+								Cart
+							</button>
+
+							<div className="flex flex-col gap-1 pb-3">
+								<p className="text-rose-300 text-sm font-medium">
+									{product.category}
+								</p>
+								<p className="text-rose-500 font-bold">{product.name}</p>
+								<p className="text-rose-800 font-bold">
+									$ {product.price.toFixed(2)}
+								</p>
+							</div>
+						</div>
+					);
+				})}
+
+				<div className="flex flex-col items-center gap-2 p-6 bg-white">
+					{' '}
+					<h2 className="text-rose-800 font-bold text-2xl self-start">
+						Your Cart <span>(0)</span>
+					</h2>
+					<img src={emptyCartIcon} alt="" />
+					<p className="text-rose-500 font-semibold">
+						Your added items will appear here
+					</p>
+				</div>
+			</div>
+		</div>
 	);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
+export const content = ['./src/**/*.{html,js,jsx,ts,tsx}'];
+export const theme = {
+	extend: {
+		colors: {
+			red: 'hsl(14, 86%, 42%)',
+			green: 'hsl(159, 69%, 38%)',
+			rose: {
+				50: 'hsl(20, 50%, 98%)',
+				100: 'hsl(13, 31%, 94%)',
+				300: 'hsl(14, 25%, 72%)',
+				400: 'hsl(7, 20%, 60%)',
+				500: 'hsl(12, 20%, 44%)',
+				900: 'hsl(14, 65%, 9%)',
+			},
+		},
+		fontFamily: {
+			sans: ['Red Hat Text', 'sans-serif'],
+		},
+		fontsize: {
+			product: '16px',
+		},
 	},
-	plugins: [],
 };
+export const plugins = [];
