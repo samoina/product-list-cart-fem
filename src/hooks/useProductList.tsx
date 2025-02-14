412fb0ca70e3ba226542c55d7af0489ec99b19d0
Samoina <samoina.w@gmail.com>
2025-02-14 17:20:24 +0300

    fix: change link depending on the environment

--- src/hooks/useProductList.tsx
+++ src/hooks/useProductList.tsx
@@ -1,8 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 
 //this is the actual function that does the fetching
+
+const isProd = import.meta.env.PROD;
 const fetchProducts = async () => {
-	const response = await fetch('http://localhost:8000/api/products');
+	const response = await fetch(
+		isProd
+			? 'https://product-list-cart-server.onrender.com/api/products'
+			: 'http://localhost:8000/api/products'
+	);
 	// const response = await fetch('https://dummyjson.com/products');
 	console.log('enter useQuery fetch func');
 	if (!response.ok) {
